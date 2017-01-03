a67eac3a12ee24244325c4a2e22cdb56ee1e33f0
Jeff Bentler <bentljm@gmail.com>
2017-01-02 19:26:51 -0800

    Setup refresh button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,16 +2,16 @@
 $(document).ready(function() {
 
   // var message = {
-  //   username: 'shawndrost',
-  //   text: 'trololo',
-  //   roomname: '4chan'
+  //   username: 'cats',
+  //   text: 'meowmeowmeowmeowmeow',
+  //   roomname: 'room full of cats'
   // };
 
   // $.ajax({
   //   // This is the url you should use to communicate with the parse API server.
   //   url: 'https://api.parse.com/1/classes/messages',
   //   type: 'POST',
-  //   data: escape(JSON.stringify(message)),
+  //   data: JSON.stringify(message),
   //   contentType: 'application/json',
   //   success: function (data) {
   //     console.log('chatterbox: Message sent');
@@ -46,47 +46,32 @@ $(document).ready(function() {
 
   });
 
+  $('.newMessage').on('click', function () {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message posted', data);
+        var index = 0;
+        while (index < data.results.length) {
+          var message = data.results[index].text;
+          var $message = $('<div></div>');
+          $message.text(data.results[index].username + ': ' + message);
+          $('#chats').append($message);
+          index++;
+        }
+      },
+      error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+
+        console.error('chatterbox: Failed to post message', data);
+      }
+
+    });
+  });
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'https://api.parse.com/1/classes/messages',
-//   type: 'POST',
-//   data: escape(JSON.stringify(message)),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message', data);
-//   }
-// });
-
-
-
-
-
-
 
 
